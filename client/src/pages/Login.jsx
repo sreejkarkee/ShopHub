aec571267a143c95dea08849252201d8c2c6277f
SriJan <srijan1769@gmail.com>
2026-08-30 12:34:29 +0545

    ..

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from '../api/axios';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../contexts/AuthContext';
 
 export default function Login() {
   const [form, setForm] = useState({ email: '', password: '' });
