1fac4740d66c86e7699455f10bfb1dda76cd72bb
SriJan <srijan1769@gmail.com>
2026-08-30 12:28:11 +0545

    ..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './contexts/AuthContext';
 import PrivateRoute from './components/PrivateRoute';
-
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ProductList from './pages/customer/ProductList';
